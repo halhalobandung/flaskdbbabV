345a30cde073b78981be9f2f3a6bd82196fc5626
halhalobandung <anta3406@gmail.com>
2025-11-02 20:42:04 +0700

    database api

--- myapp.py
+++ myapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, request, redirect, url_for
+from flask import Flask, render_template, session, request, redirect, url_for, jsonify
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -9,7 +9,8 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'flaskmysql'
-Mysql = MySQL (app)
+
+mysql = MySQL (app)
 
 @app.route('/')
 def index():
@@ -20,7 +21,7 @@ def login():
     if request.method == 'POST' and 'iniemail' in request.form and 'inipassword' in request.form:
         email = request.form ['iniemail']
         password = request.form ['inipassword'] 
-        cur = Mysql.connection.cursor()
+        cur = mysql.connection.cursor()
         cur.execute ("SELECT * FROM users where email = %s and password = %s", (email, password))
         result = cur.fetchone()
         if result:
@@ -35,7 +36,7 @@ def login():
 @app.route('/home')
 def home():
     if 'is_logged_in' in session:
-        cur = Mysql.connection.cursor()
+        cur = mysql.connection.cursor()
         cur.execute ("SELECT * FROM users")
         data = cur.fetchall()
         cur.close()
@@ -48,6 +49,29 @@ def logout():
     session.pop('is_logged_in', None)
     session.pop('username', None)
     return redirect(url_for ('login'))
+
+@app.route('/api/create_users', methods=['POST'])
+def create_users():
+    data = request.get_json()
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+    alamat = data.get('alamat')
+    notelp = data.get('notelp')
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO users(username, email, password, alamat, notelp) VALUES(%s, %s, %s, %s, %s)", (username, email, password, alamat, notelp))
+    mysql.connection.commit()
+    cur.close()
+    return jsonify({'massage': 'User created successfully'}), 201
+
+@app.route('/api/get_users', methods=['GET'])
+def get_users():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT username, email, password FROM users")
+    users = cur.fetchall()
+    cur.close()
+    user_list = [{'username': user[0], 'email': user[1], 'password': user[2]} for user in users]
+    return jsonify(user_list), 200 
 
 if __name__ == '__main__':
     app.run (debug=True)
